93533aea9206e3607c41591bcb7fda70b827d48a
AJ De Guzman <aj.deguzman@ist-systems.com>
2022-11-25 09:34:05 +0000

    ongoing dev

--- webapp/controller/InterplantTransferDC.controller.js
+++ webapp/controller/InterplantTransferDC.controller.js
@@ -30,11 +30,178 @@ sap.ui.define([
         return Controller.extend("zuioutdel.controller.InterplantTransferDC", {
             onInit: function () {
                 _this = this;
+                _this.showLoadingDialog("Loading...");
+
+                this._aColumns = {};
+
+                // Initialize router
+                var oComponent = this.getOwnerComponent();
+                this._router = oComponent.getRouter();
+                this._router.getRoute("RouteCreateManualPO").attachPatternMatched(this._routePatternMatched, this);
+
                 this.initializeComponent();
             },
 
             initializeComponent() {
                 this.getCaption();
+                this.getColumns();
+
+                this.closeLoadingDialog();
+            },
+
+            getColumns: async function() {
+                var oModelColumns = new JSONModel();
+                var sPath = jQuery.sap.getModulePath("zuioutdel", "/model/columns.json")
+                await oModelColumns.loadData(sPath);
+
+                var oColumns = oModelColumns.getData();
+                var oModel = this.getOwnerComponent().getModel();
+
+                oModel.metadataLoaded().then(() => {
+                    this.getDynamicColumns(oColumns, "OUTDELDLVDTLHUMOD", "ZDV_OUTDELDLVHU");
+                    
+                    setTimeout(() => {
+                        this.getDynamicColumns(oColumns, "OUTDELDLVDTLMOD", "ZDV_OUTDELDLV");
+                    }, 100);
+
+                    setTimeout(() => {
+                        this.getDynamicColumns(oColumns, "OUTDELSTATOVWMOD", "ZDV_OUTDELSTAT");
+                    }, 100);
+
+                    setTimeout(() => {
+                        this.getDynamicColumns(oColumns, "OUTDELMATDOCMOD", "ZDV_OUTDELDOC");
+                    }, 100);
+                })
+            },
+
+            getDynamicColumns(arg1, arg2, arg3) {
+                var oColumns = arg1;
+                var modCode = arg2;
+                var tabName = arg3;
+
+                var oJSONColumnsModel = new JSONModel();
+                var vSBU = this.getView().getModel("ui").getData().activeSbu;
+
+                var oModel = this.getOwnerComponent().getModel("ZGW_3DERP_COMMON_SRV");
+                oModel.setHeaders({
+                    sbu: vSBU,
+                    type: modCode,
+                    tabname: tabName
+                });
+                
+                oModel.read("/ColumnsSet", {
+                    success: function (oData, oResponse) {
+                        oJSONColumnsModel.setData(oData);
+
+                        if (oData.results.length > 0) {
+                            if (modCode === 'OUTDELHDRMOD') {
+                                var aColumns = _this.setTableColumns(oColumns["outDelHdr"], oData.results);                          
+                                _this._aColumns["outDelHdr"] = aColumns["columns"];
+                                _this.addColumns(_this.byId("outDelHdrTab"), aColumns["columns"], "outDelHdr");
+                            }
+                            else if (modCode === 'OUTDELDTLMOD') {
+                                var aColumns = _this.setTableColumns(oColumns["outDelDtl"], oData.results);                         
+                                _this._aColumns["outDelDtl"] = aColumns["columns"];
+                                _this.addColumns(_this.byId("outDelDtlTab"), aColumns["columns"], "outDelDtl");
+                            }
+                        }
+                    },
+                    error: function (err) {
+                        _this.closeLoadingDialog();
+                    }
+                });
+            },
+
+            setTableColumns: function(arg1, arg2) {
+                var oColumn = (arg1 ? arg1 : []);
+                var oMetadata = arg2;
+                
+                var aColumns = [];
+
+                oMetadata.forEach((prop, idx) => {
+                    var vCreatable = prop.Creatable;
+                    var vUpdatable = prop.Editable;
+                    var vSortable = true;
+                    var vSorted = prop.Sorted;
+                    var vSortOrder = prop.SortOrder;
+                    var vFilterable = true;
+                    var vName = prop.ColumnLabel;
+                    var oColumnLocalProp = oColumn.filter(col => col.name.toUpperCase() === prop.ColumnName);
+                    var vShowable = true;
+                    var vOrder = prop.Order;
+
+                    //columns
+                    aColumns.push({
+                        name: prop.ColumnName, 
+                        label: vName, 
+                        position: +vOrder,
+                        type: prop.DataType,
+                        creatable: vCreatable,
+                        updatable: vUpdatable,
+                        sortable: vSortable,
+                        filterable: vFilterable,
+                        visible: prop.Visible,
+                        required: prop.Mandatory,
+                        width: prop.ColumnWidth + 'px',
+                        sortIndicator: vSortOrder === '' ? "None" : vSortOrder,
+                        hideOnChange: false,
+                        valueHelp: oColumnLocalProp.length === 0 ? {"show": false} : oColumnLocalProp[0].valueHelp,
+                        showable: vShowable,
+                        key: prop.Key === '' ? false : true,
+                        maxLength: prop.Length,
+                        precision: prop.Decimal,
+                        scale: prop.Scale !== undefined ? prop.Scale : null
+                    })
+                })
+
+                aColumns.sort((a,b) => (a.position > b.position ? 1 : -1));
+                var aColumnProp = aColumns.filter(item => item.showable === true);
+
+                return { columns: aColumns };
+            },
+
+            addColumns(table, columns, model) {
+                var aColumns = columns.filter(item => item.showable === true)
+                aColumns.sort((a,b) => (a.position > b.position ? 1 : -1));
+
+                aColumns.forEach(col => {
+                    // console.log(col)
+                    if (col.type === "STRING" || col.type === "DATETIME") {
+                        table.addColumn(new sap.ui.table.Column({
+                            id: model + "Col" + col.name,
+                            width: col.width,
+                            sortProperty: col.name,
+                            filterProperty: col.name,
+                            label: new sap.m.Text({text: col.label}),
+                            template: new sap.m.Text({text: "{" + model + ">" + col.name + "}"}),
+                            visible: col.visible
+                        }));
+                    }
+                    else if (col.type === "NUMBER") {
+                        table.addColumn(new sap.ui.table.Column({
+                            id: model + "Col" + col.name,
+                            width: col.width,
+                            hAlign: "End",
+                            sortProperty: col.name,
+                            filterProperty: col.name,
+                            label: new sap.m.Text({text: col.label}),
+                            template: new sap.m.Text({text: "{" + model + ">" + col.name + "}"}),
+                            visible: col.visible
+                        }));
+                    }
+                    else if (col.type === "BOOLEAN" ) {
+                        table.addColumn(new sap.ui.table.Column({
+                            id: model + "Col" + col.name,
+                            width: col.width,
+                            hAlign: "Center",
+                            sortProperty: col.name,
+                            filterProperty: col.name,                            
+                            label: new sap.m.Text({text: col.label}),
+                            template: new sap.m.CheckBox({selected: "{" + model + ">" + col.name + "}", editable: false}),
+                            visible: col.visible
+                        }));
+                    }
+                })
             },
 
             getCaption() {
@@ -58,7 +225,23 @@ sap.ui.define([
                 oDDTextParam.push({CODE: "RCVSLOC"});
                 oDDTextParam.push({CODE: "ETD"});
                 oDDTextParam.push({CODE: "ETA"});
-                oDDTextParam.push({CODE: "MODESHIP"});             
+                oDDTextParam.push({CODE: "MODESHIP"});       
+                
+                oDDTextParam.push({CODE: "VESSEL"});
+                oDDTextParam.push({CODE: "CONTAINERNO"});
+                oDDTextParam.push({CODE: "HBL"});
+                oDDTextParam.push({CODE: "MBL"});
+                oDDTextParam.push({CODE: "NOPACK"});
+                oDDTextParam.push({CODE: "CREATEDBY"});
+                oDDTextParam.push({CODE: "CREATEDDT"});    
+
+                oDDTextParam.push({CODE: "FORWARDER"});
+                oDDTextParam.push({CODE: "CARRIER"});
+                oDDTextParam.push({CODE: "REFDOCNO"});
+                oDDTextParam.push({CODE: "REFDOCDT"});
+                oDDTextParam.push({CODE: "DELETED"});
+                oDDTextParam.push({CODE: "UPDATEDBY"});
+                oDDTextParam.push({CODE: "UPDATEDDT"});
 
                 // MessageBox
                 // oDDTextParam.push({CODE: "INFO_NO_SELECTED"});
